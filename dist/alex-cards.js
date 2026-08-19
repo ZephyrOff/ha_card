efe9e2f6cfc799321d8812147315cb4012890488
ZephyrOff <pajak.alexandre@gmail.com>
2026-08-19 21:24:13 +0200

    v0.8.3

--- dist/alex-cards.js
+++ dist/alex-cards.js
@@ -6,7 +6,7 @@
  * (classe + éditeur + customElements.define + window.customCards.push).
  */
 
-const ALEX_CARDS_VERSION = "0.8.2";
+const ALEX_CARDS_VERSION = "0.8.3";
 
 console.info(
   `%c ALEX-CARDS %c v${ALEX_CARDS_VERSION} `,
